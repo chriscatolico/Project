68bb6f3944084734e69250e0cd7fe48fb9eab193
Andrew D <bizkit4521@gmail.com>
2019-03-30 10:28:08 -0700

    updated education data

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-api_key = 'gb_wrkilRgmDegSZJcEufnWcS4R3tgMTbVc0DhWM72rGUokjmLyY-XAl1OpxDHor-UWrFdzyGWbmnMTB_Tu8e3REmVLj8YwABWczjUeat_m59j_dEOajGYRP0wuYXHYx'
+yelp_api_key = 'gb_wrkilRgmDegSZJcEufnWcS4R3tgMTbVc0DhWM72rGUokjmLyY-XAl1OpxDHor-UWrFdzyGWbmnMTB_Tu8e3REmVLj8YwABWczjUeat_m59j_dEOajGYRP0wuYXHYx'
+
+google_api_key = 'AIzaSyD2X0p2LPK0TMZJyLm7uzzt45uhbBlXEE4'
